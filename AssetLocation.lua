4983262a8b23475283da4d6af9d2753dd9f03c0c
traissa <tara.raissa@gmail.com>
2014-04-07 11:50:25 +0700

    update

--- AssetLocation.lua
+++ AssetLocation.lua
@@ -1,3 +1,6 @@
+-- game = 30 fps
+-- 200 ms == 6 frame
+
 sprite = {}
 
 
@@ -10,7 +13,7 @@ sprite[1].sheetData = {	numFrames = 4*3 ,
 						width = 640/4,
 						height = 510/3,
 						}
-sprite[1].sequenceData = {name = "playerRun", start = 1, count = 12, time = 1000, loopCount = 0}
+sprite[1].sequenceData = {name = "playerRun", start = 1, count = 12, time = 400, loopCount = 0}
 
 
 sprite[2] = {}
@@ -22,7 +25,7 @@ sprite[2].sheetData = {	numFrames = 4*3 ,
 						width = 2560/4,
 						height = 1860/3,
 						}
-sprite[2].sequenceData = {name = "playerWalk", start = 1, count = 12, time = 1000, loopCount = 0}
+sprite[2].sequenceData = {name = "playerWalk", start = 1, count = 12, time = 400, loopCount = 0}
 
 sprite[3] = {}
 sprite[3].name = "coinRed"
@@ -33,7 +36,7 @@ sprite[3].sheetData = {	numFrames = 3*2,
 						width = 105/3,
 						height = 80/2,
 						}
-sprite[3].sequenceData = {name = "coinRed", start = 1, count = 6, time = 1000, loopCount = 0}
+sprite[3].sequenceData = {name = "coinRed", start = 1, count = 6, time = 200, loopCount = 0}
 
 sprite[4] = {}
 sprite[4].name = "coinYellow"
@@ -44,7 +47,7 @@ sprite[4].sheetData = {	numFrames = 3*2,
 						width = 105/3,
 						height = 80/2,
 						}
-sprite[4].sequenceData = {name = "coinYellow", start = 1, count = 6, time = 1000, loopCount = 0}
+sprite[4].sequenceData = {name = "coinYellow", start = 1, count = 6, time = 200, loopCount = 0}
 
 sprite[5] = {}
 sprite[5].name = "crack"
@@ -55,7 +58,7 @@ sprite[5].sheetData = {	numFrames = 6*1,
 						width = 3840/6,
 						height = 1280/1,
 						}
-sprite[5].sequenceData = {name = "crack", start = 1, count = 6, time = 1000, loopCount = 1}
+sprite[5].sequenceData = {name = "crack", start = 1, count = 6, time = 200, loopCount = 1}
 
 sprite[6] = {}
 sprite[6].name = "blood"
@@ -66,11 +69,36 @@ sprite[6].sheetData = {	numFrames = 6*1,
 						width = 3840/6,
 						height = 1280/1,
 						}
-sprite[6].sequenceData = {name = "blood", start = 1, count = 6, time = 1000, loopCount = 1}
+sprite[6].sequenceData = {name = "blood", start = 1, count = 6, time = 200, loopCount = 1}
+
+
+
 
 imageLocation = {}
 
 imageLocation.floor = "Asset/Bg2DFloor.png"
 
 imageLocation.background2D = "Asset/Bg2DMatte.png"
+
+imageLocation.floor3D = "Asset/Bg3DGroundTile.png"
+
+imageLocation.background3D = "Asset/Bg3DMatte.png"
+
+imageLocation.scoreBoard = "Asset/BrdScore.png"
+
+imageLocation.iPhone = "Asset/Obj3DiPhone.png"
+
+
+imageLocation.button = {
+	play = "Asset/BtnPlay.png",
+	okay = "Asset/BtnOkay.png",
+	share = "Asset/BtnShre.png",
+	rate = "Asset/BtnRate.png",
+}
+
+imageLocation.text = {
+	bumped = "Asset/TxtBumpedHdln.png"
+	crushed = "Asset/TxtCrunchedHdln.png"
+	fired = "Asset/TxtFiredHdln.png"
+}
 
